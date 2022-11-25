1f9216b0aa9c0770a60aa50e5d3a551bd5372755
rakeshkm2 <rakesh.km@wyzebulb.com>
2022-11-25 18:47:21 +0530

    feat: :art: commiting the code till the Create-Order-History-Screen

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -2,23 +2,31 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className="card">
       <Link legacyBehavior href={`/product/${product.slug}`}>
-        <img
-          src={product.image}
-          alt={product.name}
-          className="rounded shadow"
-        />
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5">
         <Link legacyBehavior href={`/product/${product.slug}`}>
-          <h2 className="text-lg">{product.name}</h2>
+          <a>
+            <h2 className="text-lg">{product.name}</h2>
+          </a>
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
-        <button className="primary-button" type="button">
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
           Add to cart
         </button>
       </div>
--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [], shippingAddress: {} },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
@@ -40,6 +40,8 @@ function reducer(state, action) {
           paymentMethod: '',
         },
       };
+    case 'CART_CLEAR_ITEMS':
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
     case 'SAVE_SHIPPING_ADDRESS':
       return {
         ...state,
@@ -49,6 +51,14 @@ function reducer(state, action) {
             ...state.cart.shippingAddress,
             ...action.payload,
           },
+        },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          paymentMethod: action.payload,
         },
       };
     default:
